65318663a915f2b632da34c63a1c7a7d33b8393d
Noel De Martin <noel@moodle.com>
2023-11-20 17:41:13 +0100

    Implement Desktop Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,15 +21,28 @@ const Header = () => {
               <Menu size={24} />
             </button>
           </ActionGroup>
+          <DesktopLogoWrapper>
+            <Logo />
+          </DesktopLogoWrapper>
           <ActionGroup>
-            <button>
+            <DesktopAction>
+                <SubscribeButton>
+                    Subscribe
+                </SubscribeButton>
+                <SubscriberLink href="/">
+                    Already a subscriber?
+                </SubscriberLink>
+            </DesktopAction>
+            <MobileAction>
               <User size={24} />
-            </button>
+            </MobileAction>
           </ActionGroup>
         </Row>
       </SuperHeader>
       <MainHeader>
-        <Logo />
+        <MobileLogoWrapper>
+          <Logo />
+        </MobileLogoWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +52,13 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media${QUERIES.laptopAndUp} {
+    background: revert;
+    color: var(--color-gray-900);
+    margin-top: 16px;
+    margin-bottom: 74px;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +85,57 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const MobileLogoWrapper = styled.div`
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopLogoWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+    font-size: 4rem;
+  }
+`;
+
+const MobileAction = styled.button`
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopAction = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+const SubscribeButton = styled(Button)`
+    padding-top: 10px;
+    padding-bottom: 6px;
+`;
+
+const SubscriberLink = styled.a`
+  color: var(--color-gray-900);
+  text-decoration: underline;
+  font-style: italic;
+  font-weight: var(--font-weight-normal);
+  font-family: var(--font-family-serif);
+  font-size: 0.875rem;
 `;
 
 export default Header;
